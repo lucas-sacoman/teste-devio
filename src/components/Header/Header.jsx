34d1c11839a26cdcdbaed9cc6a5b7dd68969022e
Lucas Sacoman <lucas.sacoman1999@gmail.com>
2021-06-24 19:16:00 -0300

    added header box-shadow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,5 +6,6 @@ export const Header = styled.header `
    width: 100%;
    height: 6rem;
    color: #fffefe;
-   background: #161616;
+   background: #533fff;
+   box-shadow: 0px 3px 1px 0px rgba(0, 0, 0, 0.8)
 `;
